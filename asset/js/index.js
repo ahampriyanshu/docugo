6fc5ed443da5c5a1b5ff44785fb955165e1e3057
ahampriyanshu <ahampriyanshu@gmail.com>
2021-04-05 15:16:31 +0530

    97 % completed

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,6 +1,8 @@
 const addressOfLenderRaw = document.getElementById("addressOfLender");
 const amountOfMortgageRaw = document.getElementById("amountOfMortgage");
+const amountOfMortgageWordsRaw = document.getElementById("amountOfMortgageWords");
 const borrowerLawFirmNameRaw = document.getElementById("borrowerLawFirmName");
+const borrowerLawyerNameRaw = document.getElementById("borrowerLawyerName");
 const closingDateRaw = document.getElementById("closingDate");
 const dateOfSigningRaw = document.getElementById("dateOfSigning");
 const fileNumberRaw = document.getElementById("fileNumber");
@@ -11,7 +13,6 @@ const nameOfBorrowerRaw = document.getElementById("nameOfBorrower");
 const municipalityOfSigningRaw = document.getElementById("municipalityOfSigning");
 const maturityDateRaw = document.getElementById("maturityDate");
 const propertyAddressRaw = document.getElementById("propertyAddress");
-const propertyAddressFeildRaw = document.getElementById("propertyAddressFeild");
 const priorityOfMortgageRaw = document.getElementById("priorityOfMortgage");
 const prepaymentProvisionsRaw = document.getElementById("prepaymentProvisions");
 const submitBtn = document.getElementById("submitBtn");
@@ -19,7 +20,9 @@ const submitBtn = document.getElementById("submitBtn");
 submitBtn.addEventListener("click", () => {
     const addressOfLender = addressOfLenderRaw.value;
     const amountOfMortgage = amountOfMortgageRaw.value;
+    const amountOfMortgageWords = amountOfMortgageWordsRaw.value;
     const borrowerLawFirmName = borrowerLawFirmNameRaw.value;
+    const borrowerLawyerName = borrowerLawyerNameRaw.value;
     const closingDate = closingDateRaw.value;
     const dateOfSigning = dateOfSigningRaw.value;
     const fileNumber = fileNumberRaw.value;
@@ -30,22 +33,20 @@ submitBtn.addEventListener("click", () => {
     const municipalityOfSigning = municipalityOfSigningRaw.value;
     const maturityDate = maturityDateRaw.value;
     const propertyAddress = propertyAddressRaw.value;
-    const propertyAddressFeild = propertyAddressFeildRaw.value;
     const priorityOfMortgage = priorityOfMortgageRaw.value;
     const prepaymentProvisions = prepaymentProvisionsRaw.value;
  
-    generate(addressOfLender, amountOfMortgage, borrowerLawFirmName, closingDate, dateOfSigning, fileNumber, instrumentNumberToDischarged, interestRate, nameOfLender, nameOfBorrower, municipalityOfSigning, maturityDate, propertyAddress, propertyAddressFeild, priorityOfMortgage, prepaymentProvisions, 1);
+    generate(nameOfBorrower, propertyAddress, nameOfLender, addressOfLender,  municipalityOfSigning, dateOfSigning, priorityOfMortgage,  closingDate, fileNumber,  borrowerLawFirmName, borrowerLawyerName, amountOfMortgageWords, amountOfMortgage, maturityDate, interestRate,  prepaymentProvisions, instrumentNumberToDischarged ,1);
   });
 
 function loadFile(url, callback) {
     PizZipUtils.getBinaryContent(url, callback);
 }
 
-function generate(addressOfLender, amountOfMortgage, borrowerLawFirmName, closingDate, dateOfSigning, fileNumber, instrumentNumberToDischarged, interestRate, nameOfLender, nameOfBorrower, municipalityOfSigning, maturityDate, propertyAddress, propertyAddressFeild, priorityOfMortgage, prepaymentProvisions, serialNumber) {
+function generate(nameOfBorrower, propertyAddress, nameOfLender, addressOfLender,  municipalityOfSigning, dateOfSigning, priorityOfMortgage,  closingDate, fileNumber,  borrowerLawFirmName, borrowerLawyerName, amountOfMortgageWords, amountOfMortgage, maturityDate, interestRate,  prepaymentProvisions, instrumentNumberToDischarged , serialNumber) {
     loadFile("asset/sample.docx", function (error, content) {
         if (error) { throw error };
 
-        // The error object contains additional information when logged with JSON.stringify (it contains a properties object containing all suberrors).
         function replaceErrors(key, value) {
             if (value instanceof Error) {
                 return Object.getOwnPropertyNames(value).reduce(function (error, key) {
@@ -58,14 +59,12 @@ function generate(addressOfLender, amountOfMortgage, borrowerLawFirmName, closin
 
         function errorHandler(error) {
             console.log(JSON.stringify({ error: error }, replaceErrors));
-
+            alert("Some error occured while parsing! Please try again");
             if (error.properties && error.properties.errors instanceof Array) {
                 const errorMessages = error.properties.errors.map(function (error) {
                     return error.properties.explanation;
                 }).join("\n");
                 console.log('errorMessages', errorMessages);
-                // errorMessages is a humanly readable message looking like this :
-                // 'The tag beginning with "foobar" is unopened'
             }
             throw error;
         }
@@ -82,6 +81,8 @@ function generate(addressOfLender, amountOfMortgage, borrowerLawFirmName, closin
         doc.setData({
             address_of_lender: addressOfLender,
             amount_of_mortgage: amountOfMortgage,
+            amount_of_mortgage_words: amountOfMortgageWords,
+            borrower_lawyer_name: borrowerLawyerName,
             borrower_law_firm_name: borrowerLawFirmName,
             closing_date: closingDate,
             date_of_signing: dateOfSigning,
@@ -93,18 +94,15 @@ function generate(addressOfLender, amountOfMortgage, borrowerLawFirmName, closin
             municipality_of_signing: municipalityOfSigning,
             maturity_date: maturityDate,
             property_address: propertyAddress,
-            property_address_feild: propertyAddressFeild,
             priority_of_mortgage: priorityOfMortgage,
             prepayment_provisions: prepaymentProvisions,
         });
         try {
-            // render the document (replace all occurences of {first_name} by John, {last_name} by Doe, ...)
-            doc.render();
+           doc.render();
         }
         catch (error) {
             // Catch rendering errors (errors relating to the rendering of the template : angularParser throws an error)
             errorHandler(error);
-            alert("Some error occured while parsing! Please try again");
         }
 
         const filename = nameOfBorrower + nameOfLender + "_" + serialNumber + ".doc";
@@ -115,4 +113,41 @@ function generate(addressOfLender, amountOfMortgage, borrowerLawFirmName, closin
         })
         saveAs(out, filename)
     })
+}
+
+const file = document.querySelector('#file');
+file.addEventListener('change', (e) => {
+  const [file] = e.target.files;
+  const { name: fileName, size } = file;
+  const fileSize = (size / 1000).toFixed(2);
+  const fileNameAndSize = `${fileName} - ${fileSize}KB`;
+  document.querySelector('.file-name').textContent = fileNameAndSize;
+});
+
+var navMenuDiv = document.getElementById("nav-content");
+var navMenu = document.getElementById("nav-toggle");
+
+document.onclick = check;
+function check(e) {
+  var target = (e && e.target);
+  if (!checkParent(target, navMenuDiv)) {
+    if (checkParent(target, navMenu)) {
+      if (navMenuDiv.classList.contains("hidden")) {
+        navMenuDiv.classList.remove("hidden");
+      } else {
+        navMenuDiv.classList.add("hidden");
+      }
+    } else {
+      navMenuDiv.classList.add("hidden");
+    }
+  }
+}
+function checkParent(t, elm) {
+  while (t.parentNode) {
+    if (t == elm) {
+      return true;
+    }
+    t = t.parentNode;
+  }
+  return false;
 }
--- sw.js
+++ sw.js
@@ -1,39 +1,81 @@
-const OFFLINE_VERSION = 1;
-const CACHE_NAME = 'offline';
-const OFFLINE_URL = 'asset/fallback.html';
+const staticCacheName = 'site-static-v1';
+const dynamicCacheName = 'site-dynamic-v1';
+const assets = [
+  './',
+  './index.html',
+  './asset/fallback.html',
+  './asset/js/FileSaver.js',
+  './asset/js/index.js',
+  './asset/js/docxtemplater.js',
+  './asset/js/parse.js',
+  './asset/js/pizzip-utils.js',
+  './asset/js/pizzip.js',
+  './asset/css/custom-forms.min.css',
+  './asset/css/tailwind.min.css',
+  './asset/css/style.css',
+  './asset/sample.csv',
+  './asset/sample.docx',
+  'asset/img/apple-icon-60x60.png',
+  'asset/img/apple-icon-72x72.png',
+  'asset/img/apple-icon-76x76.png',
+  'asset/img/apple-icon-114x114.png',
+  'asset/img/apple-icon-120x120.png',
+  'asset/img/apple-icon-144x144.png',
+  'asset/img/apple-icon-152x152.png',
+  'asset/img/apple-icon-180x180.png',
+  'asset/img/android-icon-192x192.png',
+  'asset/img/favicon-32x32.png',
+  'asset/img/favicon-96x96.png',
+  'asset/img/favicon-16x16.png',
+  'favicon.ico',
+  'asset/img/ms-icon-144x144.png',
+  'asset/img/logo.png',
+];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil((async () => {
-    const cache = await caches.open(CACHE_NAME);
-    await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
-  })());
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(staticCacheName).then(cache => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil((async () => {
-    if ('navigationPreload' in self.registration) {
-      await self.registration.navigationPreload.enable();
-    }
-  })());
-  self.clients.claim();
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName)
+        .map(key => caches.delete(key))
+      )
+    })
+  );
 });
 
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResponse = await event.preloadResponse;
-        if (preloadResponse) {
-          return preloadResponse;
-        }
-        const networkResponse = await fetch(event.request);
-        return networkResponse;
-      } catch (error) {
-        console.log('Fetch failed; returning offline page instead.', error);
-        const cache = await caches.open(CACHE_NAME);
-        const cachedResponse = await cache.match(OFFLINE_URL);
-        return cachedResponse;
+const limitCacheSize = (name, size) => {
+  caches.open(name).then(cache => {
+    cache.keys().then(keys => {
+      if (keys.length > size) {
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
       }
-    })());
-  }
+    });
+  });
+};
+
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request).then(fetchRes => {
+        return caches.open(dynamicCacheName).then(cache => {
+          cache.put(evt.request.url, fetchRes.clone());
+          limitCacheSize(dynamicCacheName, 10);
+          return fetchRes;
+        })
+      });
+    }).catch(() => {
+      if (evt.request.url.indexOf('.html') > -1) {
+        return caches.match('./asset/fallback.html');
+      }
+    })
+  );
 });
